3395730b3b2b507e4dc55215432d897c2a6fe34a
sansyangyan <yfuan.y@hotmail.com>
2026-07-14 22:03:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -86,14 +86,6 @@ document.addEventListener('keydown', (e) => {
     }
 });
 
-const menuButton = document.getElementById("menu-toggle");
-const links = document.querySelector(".links");
-
-menuButton.onclick = () => {
-
-    links.classList.toggle("show");
-
-};
 
 document.querySelectorAll(".links a").forEach(link => {
 
@@ -104,3 +96,12 @@ document.querySelectorAll(".links a").forEach(link => {
     };
 
 });
+
+const menuButton = document.getElementById("menu-toggle");
+const links = document.querySelector(".links");
+
+menuButton.onclick = () => {
+
+    links.classList.toggle("show");
+
+};
